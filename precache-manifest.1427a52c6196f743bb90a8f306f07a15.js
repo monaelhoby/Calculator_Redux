2847a41b24ba440719e4c6ed536f0090dd85dd5b
mona mohamed <monaelhoby@yahoo.com>
2019-12-11 22:16:25 +0200

    Updates

---
+++ precache-manifest.1427a52c6196f743bb90a8f306f07a15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a421eba32b2d7895a042b64b6246ab3b",
+    "url": "/my-app/Calculator_Redux/index.html"
+  },
+  {
+    "revision": "8e705c512679bf7afa43",
+    "url": "/my-app/Calculator_Redux/static/css/main.1eec283d.chunk.css"
+  },
+  {
+    "revision": "33b173535e35d78d84d3",
+    "url": "/my-app/Calculator_Redux/static/js/2.ce05e937.chunk.js"
+  },
+  {
+    "revision": "8e705c512679bf7afa43",
+    "url": "/my-app/Calculator_Redux/static/js/main.8be6c421.chunk.js"
+  },
+  {
+    "revision": "dc8c523643104467f684",
+    "url": "/my-app/Calculator_Redux/static/js/runtime-main.e504e047.js"
+  }
+]);
